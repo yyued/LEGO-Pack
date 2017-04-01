894cb1f3190f5bd8ce4c14c6616391c8b1f1204a
PonyCui <cuis@vip.qq.com>
2017-04-01 17:14:54 +0800

    Add RSA.

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 #!/usr/bin/env node
 'use strict'
+
 require('shelljs/global');
 const fs = require("fs");
 const path = require("path");
 const md5 = require("md5");
+const NodeRsa = require('node-rsa');
+
+if (process.argv.length < 4) {
+	console.log("Usage >>> legopack [./Folder/Path] [PrivateKey]")
+	exit(-1)
+}
+
 const env = path.normalize(pwd() + "/" + process.argv[2]);
-const name = path.parse(path.normalize(env)).name;
+
+const appParams = {
+	name: path.parse(path.normalize(env)).name,
+	privateKey: process.argv[3],
+};
+
+if (appParams.name == undefined || appParams.name.length == 0 || appParams.privateKey == undefined || appParams.privateKey.length === 0) {
+	console.log("Usage >>> legopack [./Folder/Path] [./PrivateKey/Path]")
+	exit(-1)
+}
+
 cd(env);
+
 if (process.platform == "win32") {
-	exec('DEL /S ../' + name + '.zip');
-	exec(path.resolve(path.normalize(process.argv[1]), '../zip.exe') + ' -r -q ../' + name + '.zip *');
+	exec('DEL /S ../' + appParams.name + '.zip');
+	exec(path.resolve(path.normalize(process.argv[1]), '../zip.exe') + ' -r -q ../' + appParams.name + '.zip *');
 }
 else {
-	rm('-f', '../' + name + '.zip');
-	exec('zip -r -q --exclude=*.zip* --exclude=*.git* --exclude=*.svn* --exclude=*.DS_Store* ../' + name + '.zip *');
+	rm('-f', '../' + appParams.name + '.zip');
+	exec('zip -r -q --exclude=*.zip* --exclude=*.git* --exclude=*.svn* --exclude=*.DS_Store* ../' + appParams.name + '.zip *');
 }
-fs.readFile('../' + name + '.zip', function(err, buf) {
-	fs.writeFile('../' + name + '.zip.hash', md5(buf), function(err) {
-		if (err) {
-			console.log(err);
-		}
-	})
+fs.readFile('../' + appParams.name + '.zip', function (err, buf) {
+	const privateKeyBuffer = fs.readFileSync(appParams.privateKey).toString();
+	const rsaKey = new NodeRsa(privateKeyBuffer, {
+		signingScheme: 'pkcs1-sha512',
+	});
+	fs.writeFileSync('../' + appParams.name + '.zip.hash', rsaKey.encryptPrivate(md5(buf), 'base64'));
+	fs.writeFileSync('../' + appParams.name + '.zip.pub', rsaKey.exportKey('pkcs8-public'));
 });
